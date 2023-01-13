5478a4baafc7bd5b931febc876c0e10e4cd99ca8
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2023-01-13 19:38:45 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BenchmarkList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Summary summary = BenchmarkRunner.Run<BenchmarkLists>();
+
+            Console.WriteLine(summary);
+        }
+    }
+    
+    public class BenchmarkLists
+    {
+        private readonly List<int> number;
+
+        public BenchmarkLists()
+        {
+            this.number = Enumerable.Range(0, 1000).ToList();
+        }
+
+        [Benchmark]
+        public bool UsingAny()
+        {
+            return this.number.Any();
+        }
+
+        [Benchmark]
+        public bool UsingCountProperty()
+        {
+            return this.number.Count>0;
+        }
+        [Benchmark] 
+        public bool UsingCountFunction()
+        {
+            return this.number.Count()>0;
+        }
+    }
+}
